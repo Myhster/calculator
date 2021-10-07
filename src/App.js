98d7c14fdae004a9e4108f9388fc9d3893bc2971
Your Name <wertzuio987@web.de>
2021-10-07 19:01:03 +0200

    displayLogic can subtract

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
   const [dotCounter, setDotCounter] = useState(0);
   const [metaCalc, setMetaCalc] = useState(0);
   const [opSign, setOpSign] = useState();
+  const [calcCount, setCalcCount] = useState(0);
   const numReg = /[1-9]/;
 
   const clear = () => {
-    return setOutput('0'), setDotCounter(0), setMetaCalc(0);
+    return [setOutput('0'), setDotCounter(0), setMetaCalc(0), setCalcCount(0)];
   };
 
   const buts = calcButtons.map((button, index) => {
@@ -54,11 +55,18 @@ function App() {
           return '0';
         } else if (prevOut === '0.') {
           return '-0.';
+        } else if (metaCalc === 0 && calcCount < 1) {
+          setMetaCalc(parseFloat(prevOut));
+          setDotCounter(0);
+          setOutput('0');
+          setCalcCount(1);
+          return null;
+        } else {
+          setMetaCalc(metaCalc - parseFloat(prevOut));
+          setDotCounter(0);
+          setOutput('0');
+          return null;
         }
-        setMetaCalc(parseFloat(prevOut));
-        setDotCounter(0);
-        setOutput('0');
-        return null;
       }
     };
     //-------------------------------------------------------------------
